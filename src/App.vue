1038e923fb98c90fa92cc0d4aba493bb77befaf2
AlessandroMungai <mungai.alessandro90@gmail.com>
2023-07-13 16:01:37 +0200

    start ex - template

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+
+  <header>
+    HEADER
+  </header>
+
+  <main>
+    MAIN
+  </main>
+
+  <footer>
+    FOOTER
+  </footer>
+
+</template>
+
+<style lang="scss">
+  @use "./assets/scss/"
+</style>
