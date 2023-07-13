989ed753ff1fceda158ec40e871cb3fcc29a0247
AlessandroMungai <mungai.alessandro90@gmail.com>
2023-07-13 17:10:19 +0200

    complete HeaderNav

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,4 +1,6 @@
 <script>
+import imgLogo from "../assets/img/dc-logo.png";
+
 export default {
   name: 'HeaderNav',
   components: {
@@ -6,7 +8,8 @@ export default {
   },
   data(){
     return {
-
+        imgLogo,
+        name: "logo-dc"
     }
   },
   methods: {
@@ -17,34 +20,116 @@ export default {
 
 <template>
 
-<div class="container">
-
     <header>
-        
-        <div class="icon-dc-header">
-            ICONA
-        </div>
 
-        <div class="nav-bar">
-            NAV-BAR
+        <div class="container">
+
+            <div class="header-contain">
+
+                <div class="icon-dc-header">
+                    <img :src="imgLogo" :alt="name">
+                </div>
+
+                <div class="nav-bar">
+                    <nav>
+
+                        <ul>
+                            
+                            <li>
+                                <a href="#">
+                                    CHARACTERS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    COMICS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    MOVIES
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    COLLECTIBLES
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    VIDEOS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    FANS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    NEWS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    SHOP
+                                </a>
+                            </li>
+
+                        </ul>
+                    </nav>
+
+                </div>
+
+            </div>
+
         </div>
 
     </header>
 
-</div>
-
 </template>
 
 <style scoped lang="scss">
+
+    header {
+        background-color: white;
+    }
     .container {
         max-width: 1200px;
         margin: 0 auto;
         padding: 0;
     }
 
-    header {
+    .header-contain {
         display: flex;
-        justify-content: space-between;
+        justify-content: space-between;        
+        align-items: center;
+    }
+
+    img {
+        width: 60px;
+        padding-top: 10px;
+    }
+
+    ul {
+        display: flex;
+        list-style-type: none;
+
+        li{
+            padding: 0 20px;
+    
+        }
+        
+        a {
+            color: black;
+            text-decoration: none;
+            font-size: 0.8rem;
+        }
     }
 
 </style>
